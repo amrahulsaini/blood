d304c0b0213b3aead7a8f061e27c42f18ea281dc
amrahulsaini <ammrahulsaini@gmail.com>
2025-10-30 12:22:56 +0530

    fix: allow external images from CloudFront CDN
    - Added remotePatterns configuration to next.config.ts
    - Enabled images from d1h684srpghjti.cloudfront.net
    - Leadership section images will now display properly

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'd1h684srpghjti.cloudfront.net',
+        pathname: '/assets/images/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
